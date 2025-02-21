b0c920703cbcbfc03bfcaa0284c533574704a488
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-02-21 12:22:27 +0530

    first commit :memo:

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <div className="font-bold text-2xl">
+      hello
+      <Button className="bg-red-500 hover:bg-gray-500 text-white">hello i am shadcn button</Button>
+    </div>
+      
+  )
+}
+
+export default App
